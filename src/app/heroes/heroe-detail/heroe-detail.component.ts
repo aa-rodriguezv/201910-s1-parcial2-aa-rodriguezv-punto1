b6c7eab18912dcdf043fdec2841ff2a620704480
aa-rodriguezv <angelrodri390@hotmail.com>
2019-04-03 18:27:57 -0500

    1 file modified

--- src/app/heroes/heroe-detail/heroe-detail.component.ts
+++ src/app/heroes/heroe-detail/heroe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HeroeService} from '../heroe.service';
 import {HeroeDetail} from '../heroe-detail';
 import {Movie} from '../movie';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe-detail',
@@ -10,9 +11,26 @@ import {Movie} from '../movie';
 })
 export class HeroeDetailComponent implements OnInit {
 
-  constructor( private heroeService: HeroeService) { }
+  constructor( private heroeService: HeroeService,
+  private route: ActivatedRoute) { }
+
+  heroeDetail: HeroeDetail;
+
+  heroe_id: number;
+
+  movies: Movie[];
 
   ngOnInit() {
+    this.heroe_id = +this.route.snapshot.paramMap.get('id');
+        if (this.heroe_id){
+        this.heroeDetail = new HeroeDetail();
+        this.getHeroeDetail();
+        }
+  }
+
+
+  getHeroeDetail(): void{
+    this.heroeDetail.getHeroeDetail(this.heroe_id).subscribe(detail => this.heroeDetail = detail);
   }
 
 }
