aab533148cc09867566696d64d1191aba9f36043
aa-rodriguezv <angelrodri390@hotmail.com>
2019-04-03 17:52:14 -0500

    1 file modified

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { HeroeService } from './heroe.service';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   declarations: [HeroesListComponent],
   providers: [HeroeService]
